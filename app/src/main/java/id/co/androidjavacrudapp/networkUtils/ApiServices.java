14d7e721c0720cfbf2ba2841f0af116250346e86
RachmanForniandi <rachmanforniandi@gmail.com>
2019-08-24 16:11:18 +0700

    feature add data user done

---
+++ app/src/main/java/id/co/androidjavacrudapp/networkUtils/ApiServices.java
@@ -0,0 +1,19 @@
+package id.co.androidjavacrudapp.networkUtils;
+
+import id.co.androidjavacrudapp.AddDataUser.models.ResponseAddData;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiServices {
+
+    @FormUrlEncoded
+    @POST("AddUser")
+    Call<ResponseAddData> addDataUser(
+            @Field("nama_user")String namaUser,
+            @Field("email_user")String emailUser,
+            @Field("no_hp_user")String noHpUser,
+            @Field("alamat")String alamat
+    );
+}
---
+++ app/src/main/java/id/co/androidjavacrudapp/networkUtils/NetworkClient.java
@@ -0,0 +1,22 @@
+package id.co.androidjavacrudapp.networkUtils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkClient {
+    //public static HttpLoggingInterceptor logging = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    public static OkHttpClient client = new OkHttpClient.Builder()
+ //           .addInterceptor(logging)
+            .build();
+
+    public static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://192.168.0.115/servercrud/index.php/ApiController/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build();
+
+    public static ApiServices service = retrofit.create(ApiServices.class);
+}
